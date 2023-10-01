8b7509c711f60c660468ebd402f9fabc9b7613f7
Panos <panoslin@foxmail.com>
2023-10-01 14:30:19 -0400

    modify: comments and variable naming in mpi version

--- tsp/tsp_mpi.py
+++ tsp/tsp_mpi.py
@@ -3,37 +3,38 @@
 # Created by panos on 9/28/23
 # IDE: PyCharm
 """
-mpiexec -n 4 python -m mpi4py.futures tsp/tsp_mpi2.py
+mpiexec -n 4 python -m mpi4py.futures tsp/tsp_mpi.py
 """
-from concurrent.futures import Future
-from functools import partial
+import math
 from itertools import permutations
 from typing import List, Tuple
-
 from mpi4py import MPI, futures
+from functools import partial
+from concurrent.futures import Future
 
 
-def travel(n, mask: int, dp: List[List[Tuple[float, List]]], weights: List[List[int]]) -> List[int]:
+def travel(
+        current_mask: int,
+        dp: List[List[Tuple[float, List]]],
+        weights: List[List[int]]
+) -> List[int]:
     """
-    each processor will need the dp of which have bit distance of 1 comparing to mask
-
+    each processor will need the dp of which have hamming distance of 1 comparing to current_mask
     """
-
-    result: list = list(dp[mask])
-
-    nodes_to_be_visited = [j for j in range(n) if mask & (1 << j)]
+    n = len(dp)
+    nodes_to_be_visited = [j for j in range(n) if current_mask & (1 << j)]
     for dest, src in permutations(nodes_to_be_visited, 2):
-        state_dest_not_visited = mask ^ (1 << dest)
-        result[dest] = min(
-            result[dest],
+        mask_dest_not_visited = current_mask ^ (1 << dest)
+        dp[current_mask][dest] = min(
+            dp[current_mask][dest],
             (
-                dp[state_dest_not_visited][src][0] + weights[src][dest],
-                dp[state_dest_not_visited][src][1] + [dest]
+                dp[mask_dest_not_visited][src][0] + weights[src][dest],
+                dp[mask_dest_not_visited][src][1] + [dest]
             ),
             key=lambda x: x[0]
         )
 
-    return result
+    return dp[current_mask]
 
 
 def callback(req: Future, mask):
@@ -54,34 +55,47 @@ if __name__ == '__main__':
 
             start_node = 0
             n = len(weights)
-            # dp[state][node]
-            # minimal cost/path given state and previous visited node
+
+            # dp[state][node] representing the minimal cost/path,
+            # visiting all nodes from bit mask `state` and last visited node is `node`
             dp: List[List[Tuple[float, List]]] = [
                 [(float('inf'), [])] * n
                 for _ in range(1 << n)
             ]
             for i in range(n):
                 # first node taken
-                # should be after a start node
-                dp[1 << i][i] = (weights[start_node][i], [i])
+                # should be after the start node
+                state_only_i_visited = 1 << i
+                dp[state_only_i_visited][i] = (weights[start_node][i], [i])
 
+            # list of submitted tasks
             reqs = []
-            count = 2
-            for mask in range(1, 1 << n):
+            concurrent_layer = 0
+            # number of concurrent tasks
+            concurrent_tasks = 1
+            for mask in range(1 << n):
                 req: Future = executor.submit(
                     travel,
-                    n=n,
-                    mask=mask,
+                    current_mask=mask,
                     dp=dp,
                     weights=weights
                 )
                 req.add_done_callback(partial(callback, mask=mask))
                 reqs.append(req)
-                count -= 1
-                if count <= 0:
+                concurrent_tasks -= 1
+                if concurrent_tasks <= 0:
+                    # wait for all submitted tasks to complete
+                    # before starting the next batch of concurrent tasks
                     futures.wait(reqs)
                     reqs = []
-                    count = 2
+                    concurrent_layer += 1
+                    if concurrent_layer <= n:
+                        # pascal triangle number
+                        concurrent_tasks = math.factorial(n) // (
+                                    math.factorial(concurrent_layer) * math.factorial(n - concurrent_layer)
+                        )
+                    else:
+                        concurrent_tasks = 1
 
             futures.wait(reqs)
 
@@ -92,4 +106,4 @@ if __name__ == '__main__':
                 f'{time.time() - start_time}\n'
                 f'*****************************************************************************************************'
             )
-        assert expected == dp[-1][0][0]
+            assert expected == dp[-1][0][0]
