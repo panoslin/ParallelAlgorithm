980bbd0dcc5fee33781a323a1de1edccdb9a3193
Panos <panoslin@foxmail.com>
2023-10-23 19:04:42 -0400

    add: generate_adjacency_matrix

--- tsp/generate_coordinates.py
+++ tsp/generate_coordinates.py
@@ -6,24 +6,51 @@ import random
 from typing import List
 
 
-def generate(n: int, x1, x2, y1, y2) -> List[List[int]]:
+def generate(
+        n: int,
+        x_axis_left_bound: int,
+        x_axis_right_bound: int,
+        y_axis_left_bound: int,
+        y_axis_right_bound: int
+) -> List[List[float]]:
     """
     generate n pairs of coordinates
     :param n:
-    :param x1:
-    :param x2:
-    :param y1:
-    :param y2:
+    :param x_axis_left_bound:
+    :param x_axis_right_bound:
+    :param y_axis_left_bound:
+    :param y_axis_right_bound:
     :return:
     """
     coordinates = []
     for _ in range(n):
-        x = random.uniform(x1, x2)
-        y = random.uniform(y1, y2)
+        x = random.uniform(x_axis_left_bound, x_axis_right_bound)
+        y = random.uniform(y_axis_left_bound, y_axis_right_bound)
         coordinates.append([x, y])
     return coordinates
 
 
+def generate_adjacency_matrix(coordinates: List[List[float]]) -> List[List[float]]:
+    """
+    generate adjacency matrix
+    :param coordinates:
+    :return:
+    """
+    adjacency_matrix = [[0 for _ in range(len(coordinates))] for _ in range(len(coordinates))]
+    for point1_idx in range(len(coordinates)):
+        for point2_idx in range(len(coordinates)):
+            distance = (
+                               (coordinates[point1_idx][0] - coordinates[point2_idx][0]) ** 2 +
+                               (coordinates[point1_idx][1] - coordinates[point2_idx][1]) ** 2
+                       ) ** 0.5
+            adjacency_matrix[point1_idx][point2_idx] = distance
+    return adjacency_matrix
+
+
 if __name__ == '__main__':
+    from pprint import pprint
+
     _ = generate(4, 0, 1, 0, 1)
-    print(_)
+    pprint(_)
+    _ = generate_adjacency_matrix(_)
+    pprint(_)
