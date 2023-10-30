4632a27539b0fcebe56bcfc583853e1c2cc42775
Panos <panoslin@foxmail.com>
2023-10-30 19:52:07 -0400

    modify: communicating using numpy array

--- tsp/tsp_mpi_pipeline.py
+++ tsp/tsp_mpi_pipeline.py
@@ -7,8 +7,11 @@ mpiexec -np 4 python tsp/tsp_mpi_pipeline.py
 """
 import traceback
 from typing import List, Tuple
-from tsp_logger import tsp_logger
+
+import numpy as np
 from mpi4py import MPI
+
+from tsp_logger import tsp_logger
 
 
 def error_catching(*args, **kwargs):
@@ -24,58 +27,51 @@ def compute_tsp(major_task_count, rank, task_queue):
     while major_task_count > 0:
         # 4. recv dependency task
         logger.debug(f"receiving ...", rank)
-        mask, path, cost = comm.recv(source=MPI.ANY_SOURCE, tag=1)
-        logger.debug(f"recv task {mask:05b} {(path, cost)}...", rank)
 
-        # logger.debug(f"{task_queue=} index={(mask - rank) // node_count}", rank)
+        data = np.empty(node_count + 3, dtype='i')
+        comm.Recv(
+            [data, MPI.INT],
+            source=MPI.ANY_SOURCE,
+            tag=1
+        )
+        cost = data[-1]
+        mask = data[-2]
+        number_visited_node = data[-3]
+        path = data[:number_visited_node]
+        # mask, path, cost = comm.recv()
+        logger.debug(f"recv task {mask:0{node_count}b} {(path, cost)}...", rank)
+
         task_idx = (mask - 1) // (processor_count * 2)
         task_queue[task_idx].append((mask, path, cost))
-        logger.debug(f"appended task {mask:05b} {(path, cost)} to queue ...", rank)
-        logger.debug(f"{locals()}", rank)
+        logger.debug(f"appended task {mask:0{node_count}b} {(path, cost)} to queue ...", rank)
+        logger.debug(f"{locals()=}", rank)
 
-        # logger.info(f"task queue {task_queue[(mask - rank) // node_count]}", rank)
-        # logger.debug(f"{task_queue[task_idx]=}", rank)
-        is_all_subtask_done = (len(task_queue[task_idx]) == len(path)) or (mask == 2 ** node_count - 1)
+        is_all_subtask_done = (len(task_queue[task_idx]) == number_visited_node) or (mask == (1 << node_count) - 1)
         if is_all_subtask_done:
             major_task_count -= 1
-            logger.debug(f"mask {mask:05b}: finish collecting dependent tasks", rank)
+            logger.debug(f"mask {mask:0{node_count}b}: finish collecting dependent tasks", rank)
             # all dependent tasks completed
             # start the current task
-
-            # optimal_path = min(task_queue[task_idx], key=lambda x: x[2])
-            # logger.info(f"optimal_path {optimal_path}", rank)
-
-            if mask == 2 ** node_count - 1:
-                logger.debug(f"{task_queue}", rank)
-                optimal_cost, optimal_path = find_optimal_path(task_queue[task_idx], 0)
-                optimal_path = optimal_path[:-1]
-                logger.debug(f"{optimal_cost=}", rank)
-                logger.debug(f"{optimal_path=}", rank)
-
+            if mask == (1 << node_count) - 1:
+                # final mask
                 # send result back to rank 0
                 logger.info(
                     f"task {mask} "
-                    f"{optimal_cost=} "
-                    f"{optimal_path=} "
+                    f"{data=} "
                     f"sent to process 0",
                     rank
                 )
-                comm.isend(
-                    (
-                        optimal_cost,
-                        optimal_path,
-                    ),
+                comm.Isend(
+                    [data, MPI.INT],
                     dest=0,
                     tag=2,
                 )
-                # return optimal_cost, optimal_path[:-1]
-                # final mask
 
             else:
                 # not final mask
                 # find the min cost path to each unvisited node
 
-                if mask == 2 ** node_count - 2:
+                if mask == (1 << node_count) - 2:
                     starting_node = 0
                 else:
                     starting_node = 1
@@ -86,23 +82,27 @@ def compute_tsp(major_task_count, rank, task_queue):
                         next_mask = mask | (1 << node)
                         next_processor = (next_mask + 1) // 2 % processor_count
                         optimal_cost, optimal_path = find_optimal_path(task_queue[task_idx], node)
-                        comm.isend(
-                            (
-                                next_mask,
-                                optimal_path,
-                                optimal_cost,
-                            ),
+                        # data = np.array(
+                        #     [optimal_path + [number_visited_node + 1, next_mask, optimal_cost]],
+                        #     'i'
+                        # )
+                        data[number_visited_node] = node
+                        data[-3] = number_visited_node + 1
+                        data[-2] = next_mask
+                        data[-1] = optimal_cost
+                        comm.Isend(
+                            [data, MPI.INT],
                             dest=next_processor,
                             tag=1,
                         )
                         logger.debug(
-                            f"task {next_mask:05b} "
-                            f"{(path + [node], cost + weights[path[-1]][node],)} "
+                            f"task {next_mask:0{node_count}b} "
+                            f"{data=} "
                             f"sent to process {next_processor}",
                             rank
                         )
     else:
-        logger.debug(f"finish all tasks", rank)
+        logger.info(f"finish all tasks", rank)
 
 
 def not_visited(mask: int, node: int):
@@ -140,24 +140,27 @@ if __name__ == '__main__':
         expected_cost: int = testcases[12][1]
 
         start_time = time.time()
-
+        # weights = np.array(weights)
         # 1. One to All broadcast: adjacency matrix
         comm.bcast(weights, root=0)
         logger.info(f"recv matrix {weights[0]}...", rank)
         node_count = len(weights)
         # 2. initiate subtasks: traverse all nodes starting from node 0
+        path = [-1] * node_count
         for node in range(1, node_count):
             mask = 1 << node
-            path = [node]
+            path[0] = node
             cost = weights[0][node]
+            number_visited_node = 1
             dest_process = (mask + 1) // 2 % processor_count
-            logger.info(f"init task {mask:05b} {(path, cost)} sent to process {dest_process}", rank)
-            comm.isend((mask, path, cost), dest=dest_process, tag=1)
+            logger.info(f"init task {mask:0{node_count}b} {(path, cost)} sent to process {dest_process}", rank)
+            data = np.array([path + [number_visited_node, mask, cost]], dtype='i')
+            comm.Isend([data, MPI.INT], dest=dest_process, tag=1)
 
         # 3. init task count integer
         task_count = 2 ** (node_count - 1) // processor_count
         step = 2 * processor_count
-        max_mask = (2 ** node_count)
+        max_mask = (1 << node_count)
         first_mask = rank * 2
         task_count = (max_mask - first_mask) // step + (rank != 0 and rank * 2 < max_mask)
         task_queue = [[] for _ in range(task_count)]
@@ -171,10 +174,15 @@ if __name__ == '__main__':
 
         # 5. receive final result
         logger.info(f"pending for final task.", rank)
-        optimal_cost, optimal_path = comm.recv(
-            source=(2 ** node_count) // 2 % processor_count,
+        data = np.empty(node_count + 3, dtype='i')
+        comm.Recv(
+            [data, MPI.INT],
+            source=(1 << node_count) // 2 % processor_count,
             tag=2
         )
+        logger.info(f"Recv {data=}", rank)
+        optimal_cost = data[-1]
+        optimal_path = data[:data[-3]]
         # 6. return final result
         logger.info(
             f'*****************************************************************************************************'
@@ -191,13 +199,13 @@ if __name__ == '__main__':
 
     else:
         # 1. recv weights: One to All Broadcast
-        weights: List[int] = comm.bcast(None, root=0)
+        weights = comm.bcast(None, root=0)
         logger.info(f"recv matrix {weights[0]}...", rank)
         # 2. init task count integer
         node_count = len(weights)
         task_count = 2 ** (node_count - 1) // processor_count
         step = 2 * processor_count
-        max_mask = (2 ** node_count)
+        max_mask = (1 << node_count)
         first_mask = rank * 2
         task_count = (max_mask - first_mask) // step + (rank != 0 and rank * 2 < max_mask)
         task_queue = [[] for _ in range(task_count)]
